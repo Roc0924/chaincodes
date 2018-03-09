775703dad088a9aeb81e8f2fd5107bbea0af1439
wangzhenpeng <1228916292@qq.com>
2018-03-09 16:51:13 +0800

    rebate chain code

--- rebate_cc/rebate_cc.go
+++ rebate_cc/rebate_cc.go
@@ -3,6 +3,6 @@
  * Author               : wangzhenpeng
  * Date                 : 2018/3/9
  * Time                 : 下午4:50
- * Description          : 
+ * Description          : rebate chain code
  */
 package rebate_cc
