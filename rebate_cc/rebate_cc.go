f381e8316ff3d1b6e37c5b1f623ea3dcf91754af
wangzhenpeng <1228916292@qq.com>
2018-03-09 16:50:43 +0800

    rebate chain code

---
+++ rebate_cc/rebate_cc.go
@@ -0,0 +1,8 @@
+/**
+ * Create with GoLand
+ * Author               : wangzhenpeng
+ * Date                 : 2018/3/9
+ * Time                 : 下午4:50
+ * Description          : 
+ */
+package rebate_cc
